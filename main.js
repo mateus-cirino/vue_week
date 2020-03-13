702f5c53fd3df4dcdef0b5b5487c8ffdb02f1872
Mateus Cirino <mateuscirinosilva32@gmail.com>
2020-03-12 21:56:05 -0300

    adicao de novos dados

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        description: 'A pair of warm, fuzzy socks'
+        description: 'A pair of warm, fuzzy socks',
+        image: './vmSocks-green-onWhite.jpg',
+        imageDescription: 'A pair of warm socks',
+        imageLink: 'www.vue.com'
     }
 });
