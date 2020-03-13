2270c7a2f574c78a38b5652654076bc51f4f2a29
Mateus Cirino <mateuscirinosilva32@gmail.com>
2020-03-13 10:34:50 -0300

    diretivas :v-for e :key

--- main.js
+++ main.js
@@ -5,6 +5,19 @@ var app = new Vue({
         description: 'A pair of warm, fuzzy socks',
         image: './vmSocks-green-onWhite.jpg',
         imageDescription: 'A pair of warm socks',
-        imageLink: 'www.vue.com'
+        imageLink: 'www.vue.com',
+        invetory: 10,
+        buy: true,
+        details: ["80% algodão", "20% poliester"],
+        variants: [
+            {
+                variantId: 1,
+                variantColor: "green"
+            },
+            {
+                variantId: 2,
+                variantColor: "blue"
+            }
+        ]
     }
 });
