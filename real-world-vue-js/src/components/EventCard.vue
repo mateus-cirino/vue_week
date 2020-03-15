ce2c9fa2dc35c7304841b61021149e17de72a09f
Mateus Cirino <mateuscirinosilva32@gmail.com>
2020-03-15 09:56:11 -0300

    Criacao do componente EventCard

---
+++ real-world-vue-js/src/components/EventCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>
+            <router-link :to="{ name: 'event-show', params: { id: 1 } }">
+                {{ title }}
+            </router-link>
+        </h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ["id"],
+        computed: {
+            title() {
+                return "Evento " + this.id;
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
